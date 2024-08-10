b22e4d127b06e03093aa03b52f6bd95b156e1e83
KonohA8484 <konoha8484@gmail.com>
2024-08-10 11:49:52 -0700

    update ui for login

--- src/Pages/Launcher.js
+++ src/Pages/Launcher.js
@@ -2,15 +2,24 @@ import React, {  } from 'react';
 import comment from '../assets/comment.png';
 import nextbtn from '../assets/nextbtn.png';
 import slidebtn from '../assets/slidebtn.png';
-
+import { useNavigate } from 'react-router-dom';
 
 const LauncherPage = () => {   
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/login'); // Navigate to the login page
+      };
 
     return (
         <div className="container-launcher">
             <img src={comment} alt="Launcher Comment" className="luncher-comment"/>
             <img src={slidebtn} alt="Launcher Slidebtn" className="luncher-slidebtn"/>
-            <img src={nextbtn} alt="Launcher Nextbtn" className="luncher-nextbtn"/>
+            <img 
+                src={nextbtn}
+                alt="Launcher Nextbtn" 
+                className="luncher-nextbtn"
+                onClick={handleClick}
+                />
         </div>
     );
 };
