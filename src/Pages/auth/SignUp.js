e4382074bec6522ac81edc8dc7b45b628e4e1bb3
KonohA8484 <konoha8484@gmail.com>
2024-08-10 08:02:18 -0700

    add component directory

---
+++ src/Pages/auth/SignUp.js
@@ -0,0 +1,13 @@
+import React, { } from "react";
+
+const SignUp = () => {   
+
+    return (
+        <div className="container">          
+         
+            
+        </div>
+    );
+};
+
+export default SignUp;
