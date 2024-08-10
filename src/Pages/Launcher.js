e38acce00a31e6dd9e1e63633bf7a7d4e7ce505b
obito-1997 <obito19970527@gmail.com>
2024-08-10 09:58:28 -0700

    luncher page complete

--- src/Pages/Launcher.js
+++ src/Pages/Launcher.js
@@ -1,11 +1,16 @@
 import React, {  } from 'react';
+import comment from '../assets/comment.png';
+import nextbtn from '../assets/nextbtn.png';
+import slidebtn from '../assets/slidebtn.png';
+
 
 const LauncherPage = () => {   
 
     return (
-        <div className="container-launcher">          
-         
-            
+        <div className="container-launcher">
+            <img src={comment} alt="Launcher Comment" className="luncher-comment"/>
+            <img src={slidebtn} alt="Launcher Slidebtn" className="luncher-slidebtn"/>
+            <img src={nextbtn} alt="Launcher Nextbtn" className="luncher-nextbtn"/>
         </div>
     );
 };
--- src/Pages/Splash.js
+++ src/Pages/Splash.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 import logo from '../app-logo.png'; // Import your logo image
 import ellipse from '../Ellipse.png';
+import ellipse2 from '../Ellipse2.png';
 
 const SplashPage = () => {
     const [fadeOut, setFadeOut] = useState(false);
@@ -21,6 +22,7 @@ const SplashPage = () => {
 
     return (
         <div className={`container ${fadeOut ? 'fade-out' : 'fade-in'}`}>
+            <img src={ellipse2} alt="Bittoken Ellipse2" className="logo-ellipse2"/>
             <img src={logo} alt="Bittoken Logo" className="logo" />
             <h1 className="title">BITTOKEN</h1>
             <img src={ellipse} alt="Bittoken Ellipse" className="logo-ellipse"/>
