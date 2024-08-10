4419027d899b44b09adc93c1b8a27fb580d8a949
hwakstar <hwakstar79@gmail.com>
2024-08-10 13:54:21 -0700

    update konoha ui

--- src/Pages/auth/Login.js
+++ src/Pages/auth/Login.js
@@ -4,8 +4,7 @@ import Input from "../../Components/Input";
 
 const Login = () => {   
 
-    return (
-      
+    return (     
       
         
         <div className="container-auth">    
@@ -13,7 +12,7 @@ const Login = () => {
           <div className="container-login">
             <div className="text-title">Log In</div>
             <div className="container-login-input">
-                <Input />
+                <Input label={'Name'} Icon={'user'} placeholder={'Enter your name'} />
             </div>
           </div>  
            
