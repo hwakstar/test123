7563786df884277febd48099a9624de5d9c7f608
obito-1997 <obito19970527@gmail.com>
2024-08-11 11:55:04 -0700

    change

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import SplashPage from './Pages/Splash';
 import LauncherPage from './Pages/Launcher'; // Import your launcher page component
 import Login from './Pages/auth/Login'; // Import login page
 import ForgetPassword from './Pages/verify/ForgetPassword';
-<<<<<<< HEAD
 import TestBottomBar from './Pages/TestBottomBar';
-=======
 import SignUp from './Pages/auth/SignUp';// Import SignUp page
 import Verification from './Pages/verify/Verification';
 import ResetPassword from './Pages/verify/ResetPassword';
 import Congratulation from './Pages/verify/Congratulation';
 
->>>>>>> bcce837ee5dbf065ce76264f171a82983902c9b7
 const App = () => {
     return (
         <Router>
@@ -23,13 +20,10 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/forgetpassword" element={<ForgetPassword/>} />
-<<<<<<< HEAD
                 <Route path="/testbottombar" element={<TestBottomBar/>} />
-=======
                 <Route path="/verify" element={<Verification/>} />
                 <Route path="/ResetPassword" element={<ResetPassword/>} />
                 <Route path="/Congratulation" element={<Congratulation/>} />
->>>>>>> bcce837ee5dbf065ce76264f171a82983902c9b7
                 {/* Add other routes as needed */}
             </Routes>
         </Router>
