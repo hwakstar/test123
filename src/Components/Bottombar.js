9f6c1673aff564e7fae645b1a4b056ea5c9f9004
hwakstar <hwakstar79@gmail.com>
2024-08-16 01:11:32 -0700

    update warning

--- src/Components/Bottombar.js
+++ src/Components/Bottombar.js
@@ -7,7 +7,7 @@ import mineImg2 from '../assets/mine2.png';
 import profileImg1 from '../assets/profile1.png';
 import profileImg2 from '../assets/profile2.png';
 // import teamImg1 from '../assets/team1.png';
-import teamImg1 from '../assets/team2.png';
+// import teamImg1 from '../assets/team2.png';
 import teamImg2 from '../assets/team2.png';
 import earnImg1 from '../assets/earn1.png';
 import earnImg2 from '../assets/earn2.png';
@@ -31,23 +31,23 @@ const BottomBar = () => {
     <div className="bottom-bar">
       <button className="bottom-bar-button" onClick={() => handleButtonClick('/profile', 'profile')}>
         <img src={activeButton === 'profile' ? profileImg1 : profileImg2} alt="Profile" className="bottom-bar-icon" />
-        <a className='bottom-bar-text'>Profile</a>
+        <p className='bottom-bar-text'>Profile</p>
       </button>
       <button className="bottom-bar-button" onClick={() => handleButtonClick('/team', 'team')}>
         <img src={activeButton === 'team' ? teamImg2 : teamImg2} alt="Team" className="bottom-bar-icon" />
-        <a className='bottom-bar-text'>Team</a>
+        <p className='bottom-bar-text'>Team</p>
       </button>
       <button className="bottom-bar-button" onClick={() => handleButtonClick('/mine', 'mine')}>
         <img src={activeButton === 'mine' ? mineImg1 : mineImg2} alt="Mine" className="bottom-bar-icon" />
-        <a className='bottom-bar-text'>Mine</a>
+        <p className='bottom-bar-text'>Mine</p>
       </button>
       <button className="bottom-bar-button" onClick={() => handleButtonClick('/earn', 'earn')}>
         <img src={activeButton === 'earn' ? earnImg1 : earnImg2} alt="Earn" className="bottom-bar-icon" />
-        <a className='bottom-bar-text'>Earn</a>
+        <p className='bottom-bar-text'>Earn</p>
       </button>
       <button className="bottom-bar-button" onClick={() => handleButtonClick('/wallet', 'wallet')}>
         <img src={activeButton === 'wallet' ? walletImg1 : walletImg2} alt="Wallet" className="bottom-bar-icon" style={{ height: "21px", width: "21px" }} />
-        <a className='bottom-bar-text' style={{ paddingTop: '4px' }}>Wallet</a>
+        <p className='bottom-bar-text' style={{ paddingTop: '4px' }}>Wallet</p>
       </button>
     </div>
   );
